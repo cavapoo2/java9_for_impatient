416d44dfeafeb4a4cc3f70070974126eb015d2c9
andyr <forpetessake@protonmail.com>
2017-11-16 20:19:13 +0000

    Create C1E1.java

---
+++ src/main/java/ch1/C1E1.java
@@ -0,0 +1,16 @@
+package ch1;
+
+import java.util.Scanner;
+
+public class C1E1 {
+    public static void main(String[] args) {
+        Scanner in =  new Scanner(System.in);
+        while(in.hasNext()) //type in CTRL-D to get out of this loop or CTRL-C
+        {
+            int value = in.nextInt();
+            System.out.printf("b%s , O%o, 0x%x\n",Integer.toBinaryString(value),value,value);
+            if (value != 0)
+                System.out.printf("Reciprocal = %a\n",1.0/(double)value);
+        }
+    }
+}
