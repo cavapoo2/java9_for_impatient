e8a0d494979f517b15cd9366e2892b28858d6892
andyr <forpetessake@protonmail.com>
2017-11-20 14:22:45 +0000

    Add files via upload

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -10,4 +10,13 @@ public class Util {
         return Arrays.stream(in).mapToDouble(Double::parseDouble).toArray();
 
     }
+    public static int[] convert2Ints(String[] in) {
+        return Arrays.stream(in).mapToInt(Integer::parseInt).toArray();
+
+    }
+    public static long[] convert2Longs(String[] in) {
+        return Arrays.stream(in).mapToLong(Long::parseLong).toArray();
+    }
+    //replace punctuation with space, then split on the white space.
+    public static String[] getArgs2(Scanner in) {return in.nextLine().replaceAll("[^A-Za-z]"," ").split("\\s+");}
 }
