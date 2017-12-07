7fddccbf04dd3f962dc3a9d8b96914f2dda61020
andyr <forpetessake@protonmail.com>
2017-12-07 16:00:51 +0000

    Add files via upload

--- src/main/java/com/andyr/impatient9/ch5/C5E5.java
+++ src/main/java/com/andyr/impatient9/ch5/C5E5.java
@@ -1,31 +1,47 @@
 package com.andyr.impatient9.ch5;
-
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class C5E5 {
     public static void main(String[] args) {
         C5E5.readWrite("src/main/resources/goodNumbers.txt","res.txt");
+        System.out.println("other method");
+        C5E5.readWrite2("src/main/resources/goodNumbers.txt","res2.txt");
 
     }
+    //not sure this is ok. if in.close throws, will out.close be closed
     public static void readWrite(String infilename,String outfilename){
+            Scanner in = null;
+            PrintWriter out = null;
         try {
-            Scanner in = new Scanner(new File(infilename)); //note need relative path here like src/main/resources/filename
-            PrintWriter out = new PrintWriter(outfilename);
+            in = new Scanner(new File(infilename)); //note need relative path here like src/main/resources/filename
+            out = new PrintWriter(outfilename);
             while(in.hasNextDouble()) {
                 double d = in.nextDouble();
                 System.out.println(d);
                 out.println(d);
             }
-            in.close();
-            out.close();
         }catch ( NullPointerException | FileNotFoundException | IllegalStateException e) {
             e.printStackTrace();
+        }finally {
+            in.close();
+            out.close();
         }
 
     }
-
+    //this is better as in the book
+    public static void readWrite2(String infilename, String outfilename) {
+        try(Scanner in = new Scanner(new File(infilename));
+        PrintWriter out = new PrintWriter(outfilename)) {
+            while(in.hasNextDouble()) {
+                double d = in.nextDouble();
+                System.out.println(d);
+                out.println(d);
+            }
+        }catch( NullPointerException | FileNotFoundException | IllegalStateException e)  {
+            e.printStackTrace();
+        }
+    }
 }
